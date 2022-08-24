93e7524c2aa50d3fcbf84976d73391151292caa1
mateosofka <mateo.gutierrez@sofka.com.co>
2022-08-24 15:26:09 -0500

    overrided findAll method remove from team repository

--- src/main/java/com/sofka/crud/tour/repositories/TeamRepository.java
+++ src/main/java/com/sofka/crud/tour/repositories/TeamRepository.java
@@ -3,7 +3,7 @@ package com.sofka.crud.tour.repositories;
 import com.sofka.crud.tour.models.Team;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.ArrayList;
+
 import java.util.Optional;
 
 public interface TeamRepository extends MongoRepository<Team, String> {
